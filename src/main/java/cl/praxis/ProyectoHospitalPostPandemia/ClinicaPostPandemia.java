11b6dbaa04279365463922f0acc461b8a9a6ba4a
LucianoMoncada <lucianomoncada@gmail.com>
2024-09-11 22:04:35 -0300

    Bug terminado

--- src/main/java/cl/praxis/ProyectoHospitalPostPandemia/ClinicaPostPandemia.java
+++ src/main/java/cl/praxis/ProyectoHospitalPostPandemia/ClinicaPostPandemia.java
@@ -1,16 +1,14 @@
 package cl.praxis.ProyectoHospitalPostPandemia;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HospitalPostPandemiaApplication {
+public class ClinicaPostPandemia {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(HospitalPostPandemiaApplication.class, args);
+		SpringApplication.run(ClinicaPostPandemia.class, args);
 	}
 
 }
